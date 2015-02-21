f4706647e0b318b16925cf9043e20c884aad0007
Jay Davis <jdman@users.noreply.github.com>
2015-02-21 13:45:35 +0000

    Structure

--- app/styles/config.rb
+++ app/styles/config.rb
@@ -1,8 +1,8 @@
 http_path = "/"
 css_dir = "."
 sass_dir = "."
-images_dir = "images"
-javascripts_dir = "js"
+images_dir = "app/img"
+javascripts_dir = "app/js"
 output_style = :compressed
 relative_assets=true
 line_comments = false
