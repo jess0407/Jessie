ce1035668cbc6e52189b1d6ddb9518778b31bd62
Jay Davis <jdman@users.noreply.github.com>
2015-02-23 10:00:07 +0000

    toggle menu function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+(function () {
+  var navMenu = $('nav ul'),
+      isDisplayed = 'is-displayed-mobile',
+      isCollapsed = 'is-collapsed-mobile';
+  var menuToggle = function () {
+    if(navMenu.hasClass(isCollapsed)){
+      navMenu.removeClass(isCollapsed);
+      navMenu.addClass(isDisplayed)
+    } else {
+      navMenu.removeClass(isDisplayed);
+      navMenu.addClass(isCollapsed);
+    }
+  };
+
+  $('#toggle').on('click', menuToggle);
+
+})();
