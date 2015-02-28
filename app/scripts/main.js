406b2bbf785aa8612e4232c4ce8383cfda42dcae
Jessie Shi <jessie.shi@me.com>
2015-02-28 13:26:55 +0000

    Initial Commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,33 +1,9 @@
 'use strict';
 
-var inviewHandler = function($ele) {
-  if ($(window).scrollTop() + $(window).height() >= $ele.offset().top) {
-      if(!$ele.attr('inview')) {
-          $ele.attr('inview', true);
-          $ele.hide().fadeIn(3000);
-      }
-  }
-};
 
 
 (function () {
-  var navMenu = $('nav ul'),
-      isDisplayed = 'is-displayed-mobile',
-      isCollapsed = 'is-collapsed-mobile';
-  var menuToggle = function () {
-    if(navMenu.hasClass(isCollapsed)){
-      navMenu.removeClass(isCollapsed);
-      navMenu.addClass(isDisplayed);
-    } else {
-      navMenu.removeClass(isDisplayed);
-      navMenu.addClass(isCollapsed);
-    }
-  };
 
-  var $proj = $('#projects');
-  $(window).scroll(function(){
-    inviewHandler($proj);
-  });
 
 
 })();
