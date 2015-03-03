bc498874c1d6bf476d4dec47d6d722de893c8331
Jessie Shi <jessie.shi@me.com>
2015-03-03 20:46:28 +0000

    updating assets | toggle menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,11 @@
 
 
 (function () {
-
+  $('#toggle').click(function (){
+      var $menu = $('nav ul'),
+          isVisible = !$menu.hasClass('is-collapsed-mobile');
+          isVisible ? $menu.addClass('is-collapsed-mobile'): $menu.removeClass('is-collapsed-mobile');
+  });
 
 
 })();
