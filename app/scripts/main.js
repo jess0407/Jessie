fcc2e31dd218a2fbd5de96fde1afa3ad39a61851
Jessie Shi <jessie.shi@me.com>
2015-03-06 17:55:20 +0000

    toggle submenu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,12 @@
-'use strict';
-
-
-
 (function () {
-  $('#toggle').click(function (){
-      var $menu = $('nav ul'),
-          isVisible = !$menu.hasClass('is-collapsed-mobile');
-          isVisible ? $menu.addClass('is-collapsed-mobile'): $menu.removeClass('is-collapsed-mobile');
-  });
+  'use strict';
+  var toggleMenu = function () {
+    var $menu = $('nav ul'),
+        isVisible = !$menu.hasClass('is-collapsed-mobile');
+        isVisible ? $menu.addClass('is-collapsed-mobile'): $menu.removeClass('is-collapsed-mobile');
 
+  };
+  $('#toggle').click(toggleMenu);
+  $('.nav__item').click(toggleMenu);
 
 })();
